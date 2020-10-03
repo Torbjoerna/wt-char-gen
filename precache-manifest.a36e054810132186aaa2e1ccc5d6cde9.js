e36249b6ef44937791d1100bf573374f4f65c78a
unknown <torbjoerna@gmail.com>
2020-10-03 16:22:54 +0200

    Updates

---
+++ precache-manifest.a36e054810132186aaa2e1ccc5d6cde9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "141bc5a8e4c8d7bf3aa6f97b0f4b1468",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a884c376e02c9516708e",
+    "url": "/static/css/main.c92b7660.chunk.css"
+  },
+  {
+    "revision": "905c7633df2d8047b370",
+    "url": "/static/js/2.ea1b3872.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ea1b3872.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a884c376e02c9516708e",
+    "url": "/static/js/main.9c7a0112.chunk.js"
+  },
+  {
+    "revision": "6a6a154cde2aaece772d",
+    "url": "/static/js/runtime-main.e2c9baf9.js"
+  }
+]);
